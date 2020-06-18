0caf4fc96c79c2c5d5463240c03d5e89282fda48
codeskyblue <codeskyblue@gmail.com>
2020-06-18 16:35:34 +0800

    fix travis build

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 # coding: utf-8
 
 import setuptools
+
+from m2r import parse_from_file
+
+
 setuptools.setup(
-    setup_requires=['pbr'], pbr=True, python_requires=">=3.6")
+    setup_requires=['pbr'], pbr=True, python_requires=">=3.6",
+    description=parse_from_file("README.md"))
