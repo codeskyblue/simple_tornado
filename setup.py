33128b1b84ba95c9df613b4312a8cad56ade3dd6
codeskyblue <codeskyblue@gmail.com>
2020-06-18 16:48:34 +0800

    fix install error

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 # coding: utf-8
 
+import os
 import setuptools
 
-from m2r import parse_from_file
+try:
+    from m2r import parse_from_file
+    _dir = os.path.dirname(os.path.abspath(__file__))
+    readme_path = os.path.join(_dir, "README.md")
+    description=parse_from_file(readme_path)
+except ImportError:
+    description=""
 
 
 setuptools.setup(
     setup_requires=['pbr'], pbr=True, python_requires=">=3.6",
-    description=parse_from_file("README.md"))
+    description=description)
